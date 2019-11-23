fd1e8c8828b5010142a5207ffeadb4450cb56333
GAyan17 <goelaya1998@gmail.com>
2019-11-24 01:40:33 +0530

    maps feature added

---
+++ blogofuser.php
@@ -0,0 +1,392 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Your Trip</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <!-- fontawesome CSS -->
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <!-- magnific CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/gijgo.min.css">
+    <!-- niceselect CSS -->
+    <link rel="stylesheet" href="css/nice-select.css">
+    <!-- slick CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        #map {
+            height: 400px;
+            width: 400px;
+        }
+    </style>
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu">
+            <div class="sub_menu">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-12 col-md-6">
+                            <div class="sub_menu_right_content">
+                                <span>Top destinations</span>
+                                <a href="#">Road Trip</a>
+                            <a href="#">Hiking</a>
+                            <a href="#">Beach</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-md-6">
+                            <div class="sub_menu_social_icon">
+                                <span><i class="flaticon-phone-call"></i>+XXX XXX XXX XXX</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main_menu_iner">
+                <div class="container">
+                    <div class="row align-items-center ">
+                        <div class="col-lg-12">
+                            <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+                                <a class="navbar-brand" href="index.php"> <img src="img/logo1.jpg" alt="logo"> </a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+    
+                                <div class="collapse navbar-collapse main-menu-item justify-content-center"
+                                    id="navbarSupportedContent">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="about.php">About</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="blog.php">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="top_place.php">Top Places</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="tour_details.php">Tour Details</a>
+                                    </li>                                    
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="sign_up.php">Sign Up/Login</a>
+                                    </li>
+                                </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Header part end-->
+
+    <!-- breadcrumb start-->
+    <section class="breadcrumb breadcrumb_bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner">
+                        <div class="breadcrumb_iner_item text-center">
+                            <h2>My Travel to Naini!</h2>
+                            <p>by Sameer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb start-->
+
+    <!-- about us css start-->
+    <section class="about_us section_padding">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="about_img">
+                        <img src="photos/kheerganga1.jpeg" alt="#">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about_img">
+                        <div id ="map"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about_text">
+                        <h5>Who we are</h5>
+                        <h2>We love to discover around the world</h2>
+                        <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged night yielding land creeping that seed appear were bearing.</p>
+                        <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged night yielding land creeping that seed appear were bearing.</p>
+                        <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged night yielding land creeping that seed appear were bearing.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about us css end-->
+
+    <!--top place start-->
+    <!-- <section class="event_part section_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="event_slider owl-carousel" >
+                        <div class="single_event_slider">
+                            <div class="row justify-content-end">
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="event_slider_content">
+                                        <h5>Upcoming Event</h5>
+                                        <h2>Maldeve - Asia</h2>
+                                        <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_event_slider">
+                            <div class="row justify-content-end">
+                                <div class="ol-lg-6 col-md-6">
+                                    <div class="event_slider_content">
+                                        <h5>Upcoming Event</h5>
+                                        <h2>Maldeve - Asia</h2>
+                                        <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_event_slider">
+                            <div class="row justify-content-end">
+                                <div class="ol-lg-6 col-md-6">
+                                    <div class="event_slider_content">
+                                        <h5>Upcoming Event</h5>
+                                        <h2>Maldeve - Asia</h2>
+                                        <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!--top place end-->
+    <!--top place start-->
+    <!-- <section class="client_review section_padding">
+        <div class="container">
+            <div class="row ">
+                <div class="col-xl-6">
+                    <div class="section_tittle">
+                        <h2>What they said</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="client_review_slider owl-carousel">
+                        <div class="single_review_slider">
+                            <p>I prefer to travel on weekdays and offseason. This helps me to discover more out of a place and also am happy with the quiet and peace around. </p>
+                            <h5> - Lakshmi Sharath</h5>
+                        </div>
+                        <div class="single_review_slider">
+                            <p>Go slow. Everything becomes cheaper and more immersive then – and a strange place starts to feel like home. Isn’t that what travel is all about? </p>
+                            <h5> - Shivya Nath</h5>
+                        </div>
+                        <div class="single_review_slider">
+                            <p>Sometimes not booking a hotel within India gives you better options. You can walk around a place, look at the hotels and then book! </p>
+                            <h5> - Mridula Dwivedi</h5>
+                        </div>
+                        <div class="single_review_slider">
+                            <p>If you want to get good deals travel in offseason, if you want good pictures travel around a local festival. Use public transport wherever available as it  helps you discover cities much better. </p>
+                            <h5> - Prasad Np</h5>
+                        </div>
+                        <div class="single_review_slider">
+                            <p>Leave Early, Sleep Early – always a tip for anyone traveling in Himalayas.</p>
+                            <h5> - Dheeraj Sharma</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+     --><!--top place end-->
+
+     <!--::gallery part start::-->
+     <!-- <section class="our_gallery section_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-6">
+                    <div class="section_tittle text-center">
+                        <h2>Our gallery</h2>
+                        <p>WWaters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card-columns">
+                        <div class="card">
+                            <a href="photos/dodital001.jpg" class="gallery_img">
+                               <img src="photos/dodital001.jpg" class="card-img-top" alt="">
+                            </a>
+                        </div>
+                        <div class="card">
+                            <a href="photos/kasolKheerganga.jpg" class="gallery_img">
+                                <img src="photos/kasolKheerganga.jpg" class="card-img-top" alt="">
+                            </a>
+                        </div>
+                        <div class="card">
+                            <a href="img/gallery/gallery_2.png" class="gallery_img">
+                                <img src="img/gallery/gallery_2.png" class="card-img-top" alt="">
+                            </a>
+                        </div>
+                        <div class="card">
+                            <a href="img/gallery/gallery_4.png" class="gallery_img">
+                                <img src="img/gallery/gallery_4.png" class="card-img-top" alt="">
+                            </a>
+                        </div>
+                        <div class="card">
+                            <a href="img/gallery/gallery_5.png" class="gallery_img">
+                                <img src="img/gallery/gallery_5.png" class="card-img-top" alt="">
+                            </a>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!--::gallery part end::-->
+
+    <!-- footer part start-->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-sm-6 col-md-5">
+                    <div class="single-footer-widget">
+                        <h4>Discover Destination</h4>
+                        <ul>
+                            <li><a href="#">Miami, USA</a></li>
+                            <li><a href="#">California, USA</a></li>
+                            <li><a href="#">London, UK</a></li>
+                            <li><a href="#">Saintmartine, Bangladesh</a></li>
+                            <li><a href="#">Mount Everast, India</a></li>
+                            <li><a href="#">Sidney, Australia</a></li>
+                            <li><a href="#">Miami, USA</a></li>
+                            <li><a href="#">California, USA</a></li>
+                            <li><a href="#">London, UK</a></li>
+                            <li><a href="#">Saintmartine, Bangladesh</a></li>
+                            <li><a href="#">Mount Everast, India</a></li>
+                            <li><a href="#">Sidney, Australia</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-footer-widget">
+                        <h4>Subscribe Newsletter</h4>
+                        <div class="form-wrap" id="mc_embed_signup">
+                            <form target="_blank"
+                                action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                                method="get" class="form-inline">
+                                <input class="form-control" name="EMAIL" placeholder="Your Email Address"
+                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '"
+                                    required="" type="email">
+                                <button class="click-btn btn btn-default text-uppercase"> <i class="far fa-paper-plane"></i>
+                                </button>
+                                <div style="position: absolute; left: -5000px;">
+                                    <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value=""
+                                        type="text">
+                                </div>
+
+                                <div class="info"></div>
+                            </form>
+                        </div>
+                        <p>Subscribe our newsletter to get update news and offers</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="single-footer-widget footer_icon">
+                        <h4>Contact Us</h4>
+                        <p>XXXX, Abc abcdef, Abc Wxyz, ABC
+                                +XXX XXX XXX XXX</p>
+                        <span>contact@your trip.com</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer part end-->
+
+    <!-- jquery plugins here-->
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- magnific js -->
+    <script src="js/jquery.magnific-popup.js"></script>
+    <!-- swiper js -->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- masonry js -->
+    <script src="js/masonry.pkgd.js"></script>
+    <!-- masonry js -->
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/gijgo.min.js"></script>
+    <!-- contact js -->
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/contact.js"></script>
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+</body>
+<script>
+    var map;
+    var service;
+    var infowindow;
+    function initMap() {
+        var location = new google.maps.LatLng(21, 78);
+        infowindow = new google.maps.InfoWindow();
+        map = new google.maps.Map(document.getElementById('map'), {center: location, zoom: 15});
+        var request = {query: 'Nainital', fields: ['name', 'geometry']};
+        var service = new google.maps.places.PlacesService(map);
+
+        service.findPlaceFromQuery(request, function(results, status) {
+            if (status == google.maps.Places.PlacesServiceStatus.OK) {
+                for (var i = 0; i <= results.length; i++) {
+                    createMarker(results[i]);
+                }
+                map.setCenter(results[0].geometry.location);
+            }
+        });
+    }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXGFyuKHleG6S0WSnhweKbchVbgei3ahg&callback=initMap">
+    
+</script>
+
+</html>
